0c53a554ef45224e7ac6e2888730e22ab34c6f97
Patrick Smith <patrick@burntcaramel.com>
2017-03-14 17:21:31 +1100

    Replace brunch with create-react-app

--- app/initialize.js
+++
@@ -1,8 +0,0 @@
-const { createElement } = require('react')
-const { render } = require('react-dom')
-import App from './components/App.js'
-
-document.addEventListener('DOMContentLoaded', function() {
-	const el = document.getElementById('app')
-	render(createElement(App, null), el)
-})
